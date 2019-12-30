a8a72ccfe2a362a3ad1a1cb1321970f1d4a451e0
Miwha Geschwind <miwhag@gmail.com>
2019-12-30 06:40:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,9 +14,9 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+        return programmer_hash[:grace_hopper]
      }
 
-return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
