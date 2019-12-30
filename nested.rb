f7b539e3d30587853bbb1e35a2cb1b0253397e41
Miwha Geschwind <miwhag@gmail.com>
2019-12-30 07:21:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages][0] << "Assembly"
+return programmer_hash
 end
